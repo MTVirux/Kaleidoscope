896323f11ef84b4247d017309a6af9db129be0ea
MTVirux <joaofraga96@outlook.com>
2025-12-11 00:36:10 +0000

    Fix duplicated windows

--- Kaleidoscope/Gui/MainWindow/MainWindow.cs
+++ Kaleidoscope/Gui/MainWindow/MainWindow.cs
@@ -6,26 +6,23 @@ namespace CrystalTerror.Gui.MainWindow
 
         public class MainWindow : Window
     {
-        private readonly string displayTitle;
-
-        public MainWindow() : base("Kaleidoscope")
+        public MainWindow() : base(GetDisplayTitle())
         {
-            // Try informational version first, fall back to assembly version
-            var asm = Assembly.GetExecutingAssembly();
-            var infoVer = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
-            var asmVer = asm.GetName().Version?.ToString();
-            var ver = !string.IsNullOrEmpty(infoVer) ? infoVer : (!string.IsNullOrEmpty(asmVer) ? asmVer : "0.0.0");
-
-            this.displayTitle = $"Kaleidoscope {ver}";
-
             Size = new System.Numerics.Vector2(400, 300);
         }
 
         public override void Draw()
         {
-            ImGui.Begin(this.displayTitle);
             ImGui.TextUnformatted("Main UI");
-            ImGui.End();
+        }
+
+        private static string GetDisplayTitle()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var infoVer = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            var asmVer = asm.GetName().Version?.ToString();
+            var ver = !string.IsNullOrEmpty(infoVer) ? infoVer : (!string.IsNullOrEmpty(asmVer) ? asmVer : "0.0.0");
+            return $"Kaleidoscope {ver}";
         }
     }
 }
